594a7d56fee7bd67d3504df10cd425ae40b3c223
DouglasSilvar <gogaplay@gmail.com>
2021-05-08 15:25:25 -0300

    Handling captured pieces

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -6,12 +6,19 @@ import boardgame.Position;
 import pieces.King;
 import pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMath {
 
     private int turn;
     private Color currentPlayer;
 
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
+
 
     public ChessMath(){
         board = new Board(8,8);
@@ -58,6 +65,10 @@ public class ChessMath {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p,target);
+        if(capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -87,6 +98,7 @@ public class ChessMath {
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup() {
